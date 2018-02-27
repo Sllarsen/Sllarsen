6463f638fde391e38b8a3edf36bad9610f568273
Sam Larsen <31904238+Sllarsen@users.noreply.github.com>
2018-02-26 21:13:42 -0500

    Add files via upload

--- SamsJavaScript.js
+++ SamsJavaScript.js
@@ -18,28 +18,33 @@ function isToggled(number)
     document.getElementById("button1").style.transform = "translateY(0px)";
     document.getElementById("button2").style.transform = "translateY(0px)";
     document.getElementById("button3").style.transform = "translateY(0px)";
+    document.getElementById("button1").style["boxShadow"] = "0 9px #999";
+    document.getElementById("button2").style["boxShadow"] = "0 9px #999";
+    document.getElementById("button3").style["boxShadow"] = "0 9px #999";
     document.getElementById("bodyAll").style.backgroundImage = "none";
     document.getElementById("allTabs").style.backgroundImage = "url('none')";
-
-
-
+    document.getElementById("button2").style.color = "black";
 
         if (number == 1)
         {
-            document.getElementById("button1").style.backgroundColor = "#A569BD";
+            document.getElementById("button1").style.backgroundColor = "#4B7F52";
             document.getElementById("button1").style.transform = "translateY(10px)";
+            document.getElementById("button1").style["boxShadow"] = "0 0 5px #999999";
         }
         else if (number == 2)
         {
           document.getElementById("allTabs").style.backgroundImage = "url('http://xboxhut.com/wp-content/uploads/2016/06/cork-board-background-template-traditional-compact.jpg')";
           document.getElementById("bodyAll").style.backgroundImage = "url('http://xboxhut.com/wp-content/uploads/2016/06/cork-board-background-template-traditional-compact.jpg')";
-          document.getElementById("button2").style.backgroundColor = "#A569BD";
+          document.getElementById("button2").style.backgroundColor = "#4B4237";
           document.getElementById("button2").style.transform = "translateY(10px)";
+          document.getElementById("button2").style.color = "white";
+          document.getElementById("button2").style["boxShadow"] = "0 0 5px #999999";
         }
         else
         {
-            document.getElementById("button3").style.backgroundColor = "#A569BD";
+            document.getElementById("button3").style.backgroundColor = "#4B7F52";
             document.getElementById("button3").style.transform = "translateY(10px)";
+            document.getElementById("button3").style["boxShadow"] = "0 0 5px #999999";
         }
 }
 //changes the background color of the picture page
