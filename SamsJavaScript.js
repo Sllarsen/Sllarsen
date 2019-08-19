b5690989079453141dec43f4999cf176e570e294
Sam Larsen <sllarsen@vt.edu>
2019-08-18 23:09:46 -0400

    updated year along with twitter removal and linkedin addition

--- SamsJavaScript.js
+++ SamsJavaScript.js
@@ -1,17 +1,15 @@
 
 //hide the elements
-function openTab(tabName)
-{
+function openTab(tabName) {
     var i;
     var x = document.getElementsByClassName("tab");
     for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
+        x[i].style.display = "none";
     }
     document.getElementById(tabName).style.display = "block";
 }
 //change the css of the active tab
-function isToggled(number)
-{
+function isToggled(number) {
     document.getElementById("button1").style.backgroundColor = "white";
     document.getElementById("button2").style.backgroundColor = "white";
     document.getElementById("button3").style.backgroundColor = "white";
@@ -25,26 +23,23 @@ function isToggled(number)
     document.getElementById("allTabs").style.backgroundImage = "url('none')";
     document.getElementById("button2").style.color = "black";
 
-        if (number == 1)
-        {
-            document.getElementById("button1").style.backgroundColor = "#4B7F52";
-            document.getElementById("button1").style.transform = "translateY(10px)";
-            document.getElementById("button1").style["boxShadow"] = "0 0 5px #999999";
-        }
-        else if (number == 2)
-        {
-          document.getElementById("allTabs").style.backgroundImage = "url('https://thumbs.dreamstime.com/b/corkboard-texture-background-20627837.jpg')";
-          document.getElementById("bodyAll").style.backgroundImage = "url('https://thumbs.dreamstime.com/b/corkboard-texture-background-20627837.jpg')";
-          document.getElementById("button2").style.backgroundColor = "#4B4237";
-          document.getElementById("button2").style.transform = "translateY(10px)";
-          document.getElementById("button2").style.color = "white";
-          document.getElementById("button2").style["boxShadow"] = "0 0 5px #999999";
-        }
-        else
-        {
-            document.getElementById("button3").style.backgroundColor = "#4B7F52";
-            document.getElementById("button3").style.transform = "translateY(10px)";
-            document.getElementById("button3").style["boxShadow"] = "0 0 5px #999999";
-        }
+    if (number == 1) {
+        document.getElementById("button1").style.backgroundColor = "#4B7F52";
+        document.getElementById("button1").style.transform = "translateY(10px)";
+        document.getElementById("button1").style["boxShadow"] = "0 0 5px #999999";
+    }
+    else if (number == 2) {
+        document.getElementById("allTabs").style.backgroundImage = "url('https://thumbs.dreamstime.com/b/corkboard-texture-background-20627837.jpg')";
+        document.getElementById("bodyAll").style.backgroundImage = "url('https://thumbs.dreamstime.com/b/corkboard-texture-background-20627837.jpg')";
+        document.getElementById("button2").style.backgroundColor = "#4B4237";
+        document.getElementById("button2").style.transform = "translateY(10px)";
+        document.getElementById("button2").style.color = "white";
+        document.getElementById("button2").style["boxShadow"] = "0 0 5px #999999";
+    }
+    else {
+        document.getElementById("button3").style.backgroundColor = "#4B7F52";
+        document.getElementById("button3").style.transform = "translateY(10px)";
+        document.getElementById("button3").style["boxShadow"] = "0 0 5px #999999";
+    }
 }
 //changes the background color of the picture page
